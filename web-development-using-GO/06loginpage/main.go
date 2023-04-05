3d0c7630ee832d8e52ba5da47e083d15886f13af
Aju Jacob <ajujacob88@gmail.com>
2023-04-05 10:41:52 +0530

    added web applications basics

--- web-development-using-GO/06loginpage/main.go
+++ web-development-using-GO/06loginpage/main.go
@@ -1,5 +1,62 @@
+//https://www.youtube.com/watch?v=YBBX1k9lDKw&list=PLve39GJ2D71yyECswi0lVaBm_gbnDRR9v&index=6
+
 package main
 
-func main() {
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
 
+func login(w http.ResponseWriter, r *http.Request) {
+	var fileName = "login.html"             //this is the file we are going to display on the browser when a request is received from browser
+	t, err := template.ParseFiles(fileName) //also manually change the import to "html/template" instead of "text/template"
+	if err != nil {
+
+		fmt.Println("Error when parsing file")
+		return
+		//panic(err)
+	}
+	err = t.ExecuteTemplate(w, fileName, nil)
+	if err != nil {
+
+		fmt.Println("Error when executing template")
+		return
+		//panic(err)
+	}
+
+}
+
+func loginSubmit(w http.ResponseWriter, r *http.Request) {
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+	//fmt.Println(username, password)
+
+	if userDataBase[username] == password {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "You are now logged in. Welcome to Home ")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "Didn't find matching credentials ")
+	}
+}
+
+var userDataBase = map[string]string{
+	"aju": "mypassword",
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/login": //todo: input boxes and button for credentials
+		login(w, r)
+	case "/login-submit": //todo: handle login credentials
+		loginSubmit(w, r)
+	default:
+		fmt.Fprintf(w, "not pres")
+	}
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe("", nil)
 }
