de447fd97bfe4fdbfb50260756e5228d58d6ddf7
ajujacob <ajujacob88@gmail.com>
2023-09-15 22:57:36 +0530

    added some pgms

--- go-basic-fundamentals-concepts/40-append-ellipses/main.go
+++ go-basic-fundamentals-concepts/40-append-ellipses/main.go
@@ -16,4 +16,26 @@ func main() {
 	nums2 = append(nums2, temp...) //by using ellipsis ...
 
 	fmt.Println("nums2-", nums2)
+
+	//an append trick to append value to the middle of a slice
+	val := []int{1, 2, 3, 7, 8, 9}
+	fmt.Println(val)
+	val = append(val, val[3:]...)
+	fmt.Println(val)
+	val = append(val[:3], 4, 5, 6)
+	fmt.Println(val)
+	fmt.Println(val[:])
+	fmt.Println(val[:9])
+
+	//anothe one
+	digi := []int{11, 12, 13, 17, 18, 19, 20, 21, 22}
+	fmt.Println(digi)
+	digi = append(digi, digi[3:]...)
+	fmt.Println(digi)
+	digi = append(digi[:3], 14, 15, 16)
+	fmt.Println(digi)
+	fmt.Println(digi[:15])
+	digi = append(digi[:6], digi[9:15]...)
+	fmt.Println(digi)
+
 }
