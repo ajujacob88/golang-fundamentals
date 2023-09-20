08b50865bbab14ee6310eaae3819a0c4b657db03
ajujacob <ajujacob88@gmail.com>
2023-09-20 13:21:31 +0530

    added some pgms

--- go-basic-fundamentals-concepts/50-decode-a-string/main.go
+++ go-basic-fundamentals-concepts/50-decode-a-string/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"strings"
+	"unicode/utf8"
+)
 
 func main() {
 	str := `Aj✊ Ja 🙏🙏
@@ -24,15 +29,42 @@ func main() {
 	}
 	fmt.Println()
 
-	rune := []rune(str)
-	fmt.Println(rune)
-	fmt.Println(string(rune[2]))
-	for i := range rune {
+	runeslic := []rune(str)
+	fmt.Println(runeslic)
+	fmt.Println(string(runeslic[2]))
+	for i := range runeslic {
 		//fmt.Println(str[i])
-		fmt.Printf("%c", rune[i])
+		fmt.Printf("%c", runeslic[i])
 
 	}
 
 	fmt.Println()
+
+	//how to decode this easily
+	//go offers us , for range loops as a way to decoding the runes automatically in a string
+
+	for _, v := range str {
+		fmt.Print(string(v))
+	}
+	fmt.Println()
+	//or
+	for _, v := range str {
+		fmt.Printf("%c", v)
+	}
+	fmt.Println()
+
+	newstr := strings.ToLower(str[:1])
+	fmt.Println(newstr)
+
+	//in the word below ω is a greek letter, so its size is 2 bytes.. inorder to convert it to upper case only that letter , we need to pass the bytes of that charcter, so it is like given below
+	//in go standard library, every package or functions deals with bytes will also have similar one with strings and vice versa, because both are almost same,,, so like strings.ToUpper, bytes.ToUpper is also present
+	word := []byte("ωa🙏🙏jU")
+
+	fmt.Println(string(word))
+	r, size := utf8.DecodeRune(word)
+	fmt.Println(r, size)
+	fmt.Printf("%c size:%v\n", r, size)
+	copy(word[:size], bytes.ToUpper(word[:size]))
+	fmt.Println(string(word))
 
 }
