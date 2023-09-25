677635416930e3c5504789bc0cb2631fb6d06c43
ajujacob <ajujacob88@gmail.com>
2023-09-25 11:59:02 +0530

    added some pgms

--- go-basic-fundamentals-concepts/59-map-as-sets-pgm-piping/main.go
+++ go-basic-fundamentals-concepts/59-map-as-sets-pgm-piping/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
 	"strings"
 )
 
@@ -11,7 +12,12 @@ import (
 
 // we can even check a website that contains the unique word,, pipe the curl command to our pgm,
 // for that run like this :  curl -s  https://www.rfc-editor.org/rfc/rfc20.txt | go run main.go outside  //give any web url , and rplace outside with the word to query
+
+// lastly added regular expressions (regexp) to eliminate the quotes, numbers, symbols etc from text. Regular expressions finds patterns in a text
 func main() {
+	//creating a regexp patterns that matches any characters but letters.
+	rx := regexp.MustCompile(`[^a-z]+`) //here ^ means do not match, a-z means a to z characters, + issame pattern 1 or more times..so this pattern means find any character except letters 1 or more times
+
 	//user input
 	args := os.Args[1:]
 	if len(args) != 1 {
@@ -26,6 +32,8 @@ func main() {
 	for in.Scan() {
 
 		word := strings.ToLower(in.Text())
+		word = rx.ReplaceAllString(word, "") //here replacing the characters found via regexp with an empty string.. this will remove anything except letters
+
 		if len(word) > 2 {
 			uniquewords[word] = true //the words will be unique words, because the maps key can have  only unique value, if 2 same words come, then only will be accepted as key,, = ture this will makes the map a set
 		}
