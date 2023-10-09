0e6cf30acbfc2905c7d8145dd9c2eb9759a98d87
ajujacob <ajujacob88@gmail.com>
2023-10-09 16:32:15 +0530

    added some pgms

--- go-basic-fundamentals-concepts/73-interface/book.go
+++ go-basic-fundamentals-concepts/73-interface/book.go
@@ -9,7 +9,7 @@ type book struct {
 
 func (b *book) print() { //Here (b book) is called the receiver...the method automatically receives the value of a type
 
-	//fmt.Println("Titlee:", b.title, "price:", b.price)
+	//fmt.Println("Title:", b.title, "price:", b.price)
 	fmt.Println("Titlee:", b.title, "price:", b.price.dollarstring())
 }
 
@@ -18,4 +18,4 @@ func (b *book) discount(d float64) {
 	return
 }
 
-//while using a type as pointer , it is a convention that writing all the methods of that type as pointer,,, if a type is passing as values, then all the methods of that type should be written like passing values... its a convention
+//while using a type as pointerr , it is a convention that writing all the methods of that type as pointer,,, if a type is passing as values, then all the methods of that type should be written like passing values... its a convention
